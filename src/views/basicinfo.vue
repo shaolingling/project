bc2d94c7d1f6216383540cb85ebfc0b1cf9a229a
shaolingling <shaolinglingjntp@youremail.com>
2017-08-17 15:09:39 +0800

    c

--- src/views/basicinfo.vue
+++ src/views/basicinfo.vue
@@ -5,15 +5,15 @@
         <div class="basic_info">
             <span>基本信息</span>
             <!--<ul>
-                            <li @click="getBasicInfo('01')">工商资料</li>
-                            <li @click="getBasicInfo('02')">新闻事件</li>
-                            <li @click="getBasicInfo('03')">涉诉事件</li>
-                            <li @click="getBasicInfo('04')">经营活动</li>
-                            <li @click="getBasicInfo('05')">拥有专利</li>
-                            <li @click="getBasicInfo('06')">交易股票</li>
-                            <li @click="getBasicInfo('07')">市场份额</li>
-                            <li @click="getBasicInfo('08')">合作企业</li>
-                        </ul>-->
+                                <li @click="getBasicInfo('01')">工商资料</li>
+                                <li @click="getBasicInfo('02')">新闻事件</li>
+                                <li @click="getBasicInfo('03')">涉诉事件</li>
+                                <li @click="getBasicInfo('04')">经营活动</li>
+                                <li @click="getBasicInfo('05')">拥有专利</li>
+                                <li @click="getBasicInfo('06')">交易股票</li>
+                                <li @click="getBasicInfo('07')">市场份额</li>
+                                <li @click="getBasicInfo('08')">合作企业</li>
+                            </ul>-->
             <el-button @click="getBasicInfo('01')">工商资料</el-button>
             <el-button @click="getBasicInfo('02')">新闻事件</el-button>
             <el-button @click="getBasicInfo('03')">涉诉事件</el-button>
@@ -33,11 +33,11 @@
             <div class="depth_info">
                 <span>深度信息</span>
                 <!--<ul>
-                                        <li @click="depanalysis('01','市场')">市场分析</li>
-                                        <li @click="depanalysis('02','产品')">产品分析 </li>
-                                        <li @click="depanalysis('03','专利')">专利分析</li>
-                                        <li @click="depanalysis('04','人才')">人才分析</li>
-                                    </ul>-->
+                                            <li @click="depanalysis('01','市场')">市场分析</li>
+                                            <li @click="depanalysis('02','产品')">产品分析 </li>
+                                            <li @click="depanalysis('03','专利')">专利分析</li>
+                                            <li @click="depanalysis('04','人才')">人才分析</li>
+                                        </ul>-->
                 <el-button @click="depanalysis('01','市场')">市场分析</el-button>
                 <el-button @click="depanalysis('02','产品')">产品分析</el-button>
                 <el-button @click="depanalysis('03','专利')">专利分析</el-button>
@@ -123,14 +123,21 @@ export default {
                     {
                         name: '树图',
                         type: 'tree',
-                         orient: 'horizontal',   // vertical horizontal
-                        rootLocation: { x: 'center', y: 50 }, // 根节点位置  {x: 100, y: 'center'}
-                        nodePadding: 1,
+                        orient: 'horizontal',   // vertical horizontal
+                        rootLocation: { x: "30%", y: '50%' }, // 根节点位置  {x: 100, y: 'center'}
+                        nodePadding: 10,
+                        layerPadding: 200,
                         itemStyle: {
                             normal: {
                                 label: {
                                     show: true,
-                                    formatter: "{b}"
+                                    formatter: "{b}",
+                                     position: 'right',
+                                    textStyle: {
+                                        color: '#000',
+                                        fontSize: 15,
+                                        fontWeight: 'bolder'
+                                    }
                                 },
                                 lineStyle: {
                                     color: '#48b',
@@ -239,6 +246,7 @@ h2 {
 
 
 
+
 /*.depth_info li {
     margin-top: 30px;
     border: 1px solid #ddd;
@@ -273,6 +281,6 @@ h2 {
 
 #tree {
     width: 100%;
-    height: 800px;
+    height: 2000px;
 }
 </style>
